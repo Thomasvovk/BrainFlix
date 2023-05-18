34d7c8792cc7ddfa835e3b6f1921d0f10f617707
Thomas Vovk <thomasvovk@icloud.com>
2023-05-18 01:22:40 -0400

    Updated comments for tablet

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,7 +5,7 @@ function Comments() {
   return (
     <section className="comments-section">
       <h2 className="comments-section__subheader">3 Comments</h2>
-      <div className="comments-section__container">
+      <div className="comments-section__block">
         <img
           className="comments-section__profile-image"
           src={profileImg}
@@ -27,6 +27,57 @@ function Comments() {
             </div>
           </label>
         </form>
+      </div>
+
+      <div className="comments-section__container">
+        <div className="comments-section__container-profile">
+          <div className="comments-section__container-profile-img"></div>
+        </div>
+        <div className="comments-section__container-info">
+          <div className="comments-section__container-block">
+            <p className="comments-section__container-name">Micheal Lyons</p>
+            <p className="comments-section__container-date">08/09/2021</p>
+          </div>
+          <p className="comments-section__container-text">
+            They BLEW the ROOF off at their last event, once everyone started
+            figuring out they were going. This is still simply the greatest
+            opening of an event I have EVER witnessed.
+          </p>
+        </div>
+      </div>
+
+      <div className="comments-section__container">
+        <div className="comments-section__container-profile">
+          <div className="comments-section__container-profile-img"></div>
+        </div>
+        <div className="comments-section__container-info">
+          <div className="comments-section__container-block">
+            <p className="comments-section__container-name">Gary Wong</p>
+            <p className="comments-section__container-date">07/15/2021</p>
+          </div>
+          <p className="comments-section__container-text">
+            Every time I see him shred I feel so motivated to get off my couch
+            and hop on my board. He’s so talented! I wish I can ride like him
+            one day so I can really enjoy myself!
+          </p>
+        </div>
+      </div>
+
+      <div className="comments-section__container">
+        <div className="comments-section__container-profile">
+          <div className="comments-section__container-profile-img"></div>
+        </div>
+        <div className="comments-section__container-info">
+          <div className="comments-section__container-block">
+            <p className="comments-section__container-name">Theodore Duncan</p>
+            <p className="comments-section__container-date">07/11/2021</p>
+          </div>
+          <p className="comments-section__container-text">
+            How can someone be so good!!! You can tell he lives for this and
+            loves to do it every day. Everytime I see him I feel instantly
+            happy! He’s definitely my favorite ever!
+          </p>
+        </div>
       </div>
     </section>
   );
