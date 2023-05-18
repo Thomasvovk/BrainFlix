533fd1492fad953f15cb67375aef8a29ed3ce912
Thomas Vovk <thomasvovk@icloud.com>
2023-05-18 17:05:33 -0400

    linked all videos from an object

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.scss";
+import { useState } from "react";
+import videoList from "./data/videos.json";
+import videoDescription from "./data/video-details.json";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import VideoDescription from "./components/VideoDescription/VideoDescription";
@@ -12,7 +15,7 @@ function App() {
       <Hero />
       <VideoDescription />
       <Comments />
-      <Videos />
+      <Videos videoList={videoList} />
     </>
   );
 }
--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,26 +1,28 @@
 import "../Videos/Videos.scss";
 
-function Videos() {
+function Videos(props) {
   return (
     <section className="videos">
       <div className="videos__list">
         <h2 className="videos__subheader">NEXT VIDEOS</h2>
         <div className="videos__container">
-          <div className="videos__item">
-            <div className="videos__block">
-              <img
-                className="videos__image"
-                src="https://i.imgur.com/5qyCZrD.jpg"
-                alt="next video image"
-              />
-            </div>
-            <div className="videos__description">
-              <p className="videos__title">
-                Become A Travel Pro In One Easy Lesson
-              </p>
-              <p className="videos__name">Todd Welch</p>
-            </div>
-          </div>
+          {props.videoList.map((video) => {
+            return (
+              <div className="videos__item">
+                <div className="videos__block">
+                  <img
+                    className="videos__image"
+                    src={video.image}
+                    alt="next video image"
+                  />
+                </div>
+                <div className="videos__description">
+                  <p className="videos__title">{video.title}</p>
+                  <p className="videos__name">{video.channel}</p>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
